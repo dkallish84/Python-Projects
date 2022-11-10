b458ee1d53da985c74017013d604d36775bc2837
dkallish84 <dkallish84@gmail.com>
2022-11-09 18:15:53 -0800

    Update to remove /yyyy from dates

--- Python-Scripts/print-file-contents.py
+++ Python-Scripts/print-file-contents.py
@@ -1,20 +1,22 @@
 import csv
 
+storage_list = []
+
 # Prompt for the filename
 filename = input("Please enter the filename: ")
 
 # Display the contents of the file
 with open(filename, mode='r', encoding='UTF-16') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter='\t')
-    # Remove '/2022' from the header row
-    header_row = next(csv_reader)
-    print(header_row)
 
-    for i in header_row:
-        if i.find("/2022") != -1:
-            print(str(i) + " contains '/2022'!")
-            i = i.replace('/2022', '')
-            print('After replace: ' + str(i))
+    for row in csv_reader:
+        # Store each row as a list to be processed
+        storage_list = row
+        # Check the range & length of the storage list
+        for i in range(len(storage_list)):
+            # If the value contains '/2022' remove it to be 'm/d' notation
+            if str(storage_list[i]).find('/2022') != -1:
+                print(str(i) + " contains '/2022'")
+                storage_list[i] = str(storage_list[i]).replace('/2022', '')
 
-    #for row in csv_reader:
-        #print(row)
+        print(storage_list)
