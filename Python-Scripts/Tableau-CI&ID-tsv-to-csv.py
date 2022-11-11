53b82580f0ba38cc9364ab4bd43333fe7529dae5
dkallish84 <dkallish84@gmail.com>
2022-11-10 18:10:18 -0800

    Removed unecessary list for removing '/yyyy'

--- Python-Scripts/Tableau-CI&ID-tsv-to-csv.py
+++ Python-Scripts/Tableau-CI&ID-tsv-to-csv.py
@@ -7,8 +7,6 @@ filename = input("Enter the filename: ")
 # Assumes first column is 'First Dimension' and third column is ''(Cases)
 cols_to_remove = [0, 2]
 cols_to_remove = sorted(cols_to_remove, reverse=True)
-# Store rows in a storage list
-storage_list = []
 # Store the rows that have been seen in a list
 row_seen = []
 # Store the cleaned data
@@ -30,14 +28,11 @@ with open(filename, mode='r', encoding='UTF-16') as input_file:
                 cleaned.append(row)
 
         for row in cleaned:
-            # Store each row as a list to be processed
-            storage_list = row
             # Check the range & length of the storage list
-            for i in range(len(storage_list)):
+            for i in range(len(row)):
                 # If the value contains '/2022' remove it to be 'm/d' notation
-                if str(storage_list[i]).find('/2022') != -1:
-                    print(str(i) + " contains '/2022'")
-                    storage_list[i] = str(storage_list[i]).replace('/2022', '')
+                if str(row[i]).find('/2022') != -1:
+                    row[i] = str(row[i]).replace('/2022', '')
             
             # Relabel the 'View By' column to blank
             if str(row[0]) == "View By":
--- Python-Scripts/print-file-contents.py
+++ Python-Scripts/print-file-contents.py
@@ -1,7 +1,5 @@
 import csv
 
-storage_list = []
-
 # Prompt for the filename
 filename = input("Please enter the filename: ")
 
@@ -10,13 +8,10 @@ with open(filename, mode='r', encoding='UTF-16') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter='\t')
 
     for row in csv_reader:
-        # Store each row as a list to be processed
-        storage_list = row
         # Check the range & length of the storage list
-        for i in range(len(storage_list)):
+        for i in range(len(row)):
             # If the value contains '/2022' remove it to be 'm/d' notation
-            if str(storage_list[i]).find('/2022') != -1:
-                print(str(i) + " contains '/2022'")
-                storage_list[i] = str(storage_list[i]).replace('/2022', '')
+            if str(row[i]).find('/2022') != -1:
+                row[i] = str(row[i]).replace('/2022', '')
 
-        print(storage_list)
+        print(row)
