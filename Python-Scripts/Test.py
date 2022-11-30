643a9ee80cd0d5f5dd5697873e1cf3e94aec2d5d
dkallish84 <dkallish84@gmail.com>
2022-11-29 17:05:12 -0800

    Creating Flask Test, updating Tableau-Symptom CLR

---
+++ Python-Scripts/Test.py
@@ -0,0 +1,65 @@
+import csv
+
+# Assumes first column is 'First Dimension'
+cols_to_remove = [0]
+cols_to_remove = sorted(cols_to_remove, reverse=True)
+# Store the rows that have been seen in a list
+row_seen = []
+# Store the cleaned data
+cleaned = []
+
+# Prompt for the filename
+filename = input("Please enter the filename: ")
+
+# Display the contents of the file
+with open(filename, mode='r', encoding='UTF-16') as input_file:
+    reader = csv.reader(input_file, delimiter='\t')
+
+    with open('Cleaned_'+filename, mode='w', newline='') as output_file:
+        writer = csv.writer(output_file)
+
+        for row in reader:
+            # Remove the specified column(s)
+            for col_index in cols_to_remove:
+                # Delete the column
+                del row[col_index]
+                # Write to the cleaned list
+                cleaned.append(row)
+
+        for row in cleaned:
+            # Relabel 'View By' to '' and '' to 'Cases'
+            if str(row[0]) == 'View By':
+                row[0] = ''
+            if str(row[1]) == '':
+                row[1] = 'Cases'
+
+            # Check if the 'View By' is already seen
+            if row[0] in row_seen:
+                continue
+            else:
+                # Add the 'View By' value to the row_seen list
+                row_seen.append(row[0])
+
+                # Skip the first row
+                if len(row_seen) > 1:
+                    # Change 'Cases' to 0 after the first row
+                    row[1] = 0
+
+                    # Print each cell after the first two
+                    for i, cell in enumerate(row):
+                        # Skip the first two cells
+                        if i < 2: continue
+
+                        # Convert any empty strings into 0
+                        # Convert any strings into floats then ints
+                        if cell == '':
+                            cell = 0
+                        else:
+                            cell = int(float(cell))
+                        # Add the values together and assign them to row[1] (Cases)
+                        row[1] = row[1] + cell
+
+                # Write the result to the file
+                writer.writerow(row)
+    
+    print("File processed and saved as Cleaned_" + filename)
